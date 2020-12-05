6353dce5c6cee7d4ad3ffa0ead24a10754834e1e
Gabriel Tiveron <gabrielmtiveron@gmail.com>
2020-12-05 14:54:14 -0300

    Add graph generator

---
+++ frontend/src/util/GraphGen.js
@@ -0,0 +1,31 @@
+const DIR = 'hehe/'
+export const genGraph = (num) => {
+  let base = []
+
+  for(let i = 0; i < num; i++){
+    let aux = {
+      id: i,
+      image: `${DIR}Network-Pipe-icon.png`,
+      shape: 'image',
+      edges: []
+    }
+
+    let edge_count = Math.floor(((Math.random() * num) + 1)/2)
+    let edge_list = []
+    
+    for(let j = 0; j < edge_count; j++){
+      let newEdge = Math.floor(Math.random() * num)
+      while(edge_list.includes(newEdge)){
+        newEdge = Math.floor(Math.random() * num)
+      }
+      edge_list.push(newEdge)
+      aux.edges.push(newEdge)
+    }
+
+    base.push(aux)
+  }
+
+  console.log(base)
+
+  //return base
+}
